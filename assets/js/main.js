c49461cfc86aa08bb36535ccdabf98e990a2879e
Amanamaabasi Akpan <amanamabasiakpan@gmail.com>
2022-09-09 16:09:17 +0100

    fixed all major bugs, updated media screens and styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,11 +39,12 @@ const skillsContent = document.getElementsByClassName('skills__content'),
     skillsHeader = document.querySelectorAll('.skills__header')
 
 function toggleSkills(){
-    let itemClass = this.parentNode.ClassName
+    let itemClass = this.parentNode.className
 
     for(i = 0; i < skillsContent.length; i++){
         skillsContent[i].className = 'skills__content skills__close'
     }
+
     if(itemClass === 'skills__content skills__close'){
         this.parentNode.className = 'skills__content skills__open'
     }
@@ -57,20 +58,21 @@ skillsHeader.forEach((el) =>{
  const tabs = document.querySelectorAll('[data-target]'),
     tabContents = document.querySelectorAll ('[data-content]')
 
-    tabs.forEach(tab =>{
-        tab.addEventListener('click', () => {
-            const target = document.querySelector(tab.dataset.target)
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
 
-            tabContents.forEach(tabContents => {
-                tabContents.classList.remove('qualification__active')
-            })
-            target.classList.add('qualification__active')
-            tab.forEach(tab =>{
-                tab.classList.remove('qualification__active')
-            })
-            tab.classList.add('qualification__active')
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
         })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
     })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
